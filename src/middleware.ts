548535184252f8580a864f6482f7b5bdbb46dc23
anmol <amostriker@gmail.com>
2025-07-06 22:16:56 +0530

    added clerk webhook endpoint as public in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/api/webhook/stripe(.*)",
+  "/api/webhook/clerk(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
